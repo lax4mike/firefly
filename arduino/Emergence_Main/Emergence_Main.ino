4b8ddb3b87f220ea59edc190350449fc07dfcb40
Mike Lambert <lax4mike@gmail.com>
2016-06-21 09:06:03 -0400

    Fixing mode 5

--- arduino/Emergence_Main/Emergence_Main.ino
+++ arduino/Emergence_Main/Emergence_Main.ino
@@ -358,45 +358,52 @@ void loop() {
 // follow the flashlight
 
 bool was_in_the_light_mode5 = true;
-int STEP_MODE5 = 100;
+int STEP_MODE5 = 32;
 
-//  Serial.println("argg!, the light!");
+Serial.println("argg!, the light!");
 
 while(analogRead(photo_pin) < photo_threshold && MODE == 5){
 
   //  check_for_mode_gun();
 
 
-
-  // blink immediately if the flashlight just went away
+  // if the flashlight just went away
   if (was_in_the_light_mode5){
 
     was_in_the_light_mode5 = false;
-  Serial.print("GO!");
+
+    Serial.println("GO!");
+
+    // delay a bit before the flash sequence
+    delay(500/clock_prescaler);
 
     long flashlight_gone = millis();
 
-    while(time_since(flashlight_gone) < STEP_MOSE5*7){
+    while(time_since(flashlight_gone) < STEP_MODE5*7){
 
-      if (time_since(flashlight_gone) < 250){
+      Serial.print(time_since(flashlight_gone));
+      Serial.print(" " );
+      Serial.println(STEP_MODE5);
+
+      if (time_since(flashlight_gone) < STEP_MODE5){
         blink(0, 1, 30, 60, BLUE);
       }
-      else if (time_since(flashlight_gone) < STEP_MOSE5*1){
+      else if (time_since(flashlight_gone) < STEP_MODE5*2){
           blink(0, 1, 30, 60, BLUE_GREEN);
       }
-      else if (time_since(flashlight_gone) < STEP_MOSE5*2){
+      else if (time_since(flashlight_gone) < STEP_MODE5*3){
           blink(0, 1, 30, 60, GREEN);
       }
-      else if (time_since(flashlight_gone) < STEP_MOSE5*3){
+      else if (time_since(flashlight_gone) < STEP_MODE5*4){
           blink(0, 1, 30, 60, GREEN_YELLOW);
       }
-      else if (time_since(flashlight_gone) < STEP_MOSE5*4){
+      else if (time_since(flashlight_gone) < STEP_MODE5*5){
           blink(0, 1, 30, 60, YELLOW);
       }
-      else if (time_since(flashlight_gone) < STEP_MOSE5*5){
+      else if (time_since(flashlight_gone) < STEP_MODE5*6){
           blink(0, 1, 30, 60, ORANGE);
       }
-      else if (time_since(flashlight_gone) < STEP_MOSE5*6){
+      else if (time_since(flashlight_gone) < STEP_MODE5*7){
           blink(0, 1, 30, 60, RED);
       }
       else {
