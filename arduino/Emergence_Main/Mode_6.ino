d240e65b627fe34f63ee2e34d2bb4cb47f8062f8
Mike Lambert <lax4mike@gmail.com>
2016-06-28 22:05:33 -0400

    Trying a 1/5 probability for mode 6 propagation

--- arduino/Emergence_Main/Mode_6.ino
+++ arduino/Emergence_Main/Mode_6.ino
@@ -2,11 +2,11 @@
 
 long last_pulse_time_m6 = 0;
 
-const int random_range = 2000;
+const int random_range = 8000;
 
 
 void mode6_in_the_light(){
-  
+
   int rand_number = mode_gun_last_cleared % 25;
 
   //Serial.println("Entering darkness");
@@ -16,7 +16,7 @@ void mode6_in_the_light(){
   for(int n = 0; n < rand_number; n++){
     int garbage = random(random_range);
   }
-  
+
   blink_and_pulse();
 
 }
@@ -33,9 +33,9 @@ void mode6_in_the_dark(){
 
   if(pulse_detected){
 
-    if(random(10) > 6){
+    if(random(5) == 1){
       blink_and_pulse();
-    }   
+    }
   }
 
 }
@@ -43,9 +43,9 @@ void mode6_in_the_dark(){
 //*********************************************************************
 
 void blink_and_pulse(){
-  
+
   // only if it's been at least 1.5 sec since the last transmit:
-  
+
   if(millis() > last_pulse_time_m6 + 3000 / clock_prescaler){
     low_power_delay(0, 128);    // 128 for propagation delay
     last_pulse_time_m6 = millis();
@@ -53,4 +53,3 @@ void blink_and_pulse(){
     low_power_delay(0, 256);
   }
 }
-
